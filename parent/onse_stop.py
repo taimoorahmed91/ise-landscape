20096215c7b1d94bb28c39b6cb1b5857ad137cf8
taimoorahmed91 <taimoorahmed91@gmail.com>
2023-03-31 23:03:19 +0000

    dacl from authz replicated

---
+++ parent/onse_stop.py
@@ -0,0 +1,23 @@
+import subprocess
+import time
+
+
+# Run the other script
+subprocess.run(["python3", "/root/ise-landscape/authz/inset_dacl.py"])
+
+
+#time.sleep(3)
+subprocess.run(["python3", "/root/ise-landscape/dacl-api/finalized-dacl-post.py"])
+
+
+
+
+
+subprocess.run(["python3", "/root/ise-landscape/dacl-api/finalized-dacl-put.py"])
+
+
+
+print("hello")
+
+
+

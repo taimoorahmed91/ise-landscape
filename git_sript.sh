edb57b5072deff25ad4a41a601d475b046c441c5
taimoorahmed91 <taimoorahmed91@gmail.com>
2023-05-18 22:50:54 +0000

    Automatic commit

--- git_sript.sh
+++ git_sript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Step 1: Move to a specific folder
+# Step 1: Move to the specific folder
 cd /root/ise-landscape
 
 # Step 2: Run 'git add .'
@@ -9,25 +9,13 @@ git add .
 # Step 3: Run 'git add *'
 git add *
 
-# Step 4: Ask for comments
-read -p "Enter commit comments: " comments
+# Step 4: Run 'git commit' with a default message
+git commit -m "Automatic commit"
 
-# Step 5: Run 'git commit -m' with the comments
-git commit -m "$comments"
-
-# Step 6: Run 'git push -u origin main'
-git push -u origin main
-
-# Step 7: Provide username
+# Step 5: Provide username and password
 username="taimoorahmed91"
-
-# Step 8: Provide password
 password="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"
 
-# Optional: Authenticate with Git using the provided credentials
-git config --global credential.helper 'cache --timeout=3600'
-echo -e "protocol=https\nhost=github.com\nusername=$username\npassword=$password" | git credential-store store
-
-# Optional: Clear the stored credentials after use (uncomment if desired)
-# git credential-store erase
+# Step 6: Run 'git push' with the provided credentials
+echo -e "$username\n$password" | git push origin main
 

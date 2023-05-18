eb20300c7efabd35a373742cf3bf13229c8d8b2a
taimoorahmed91 <taimoorahmed91@gmail.com>
2023-05-18 22:52:52 +0000

    Automatic commit

--- git_sript.sh
+++ git_sript.sh
@@ -12,10 +12,13 @@ git add *
 # Step 4: Run 'git commit' with a default message
 git commit -m "Automatic commit"
 
-# Step 5: Provide username and password
-username="taimoorahmed91"
+# Step 5: Provide password
 password="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"
 
-# Step 6: Run 'git push' with the provided credentials
-echo -e "$username\n$password" | git push origin main
+# Step 6: Run 'git push' with the provided password
+echo -e "protocol=https\nhost=github.com\nusername=taimoorahmed91\npassword=$password" | git credential-store store
+git push -u origin main
+
+# Optional: Clear the stored credentials after use (uncomment if desired)
+# git credential-store erase
 
